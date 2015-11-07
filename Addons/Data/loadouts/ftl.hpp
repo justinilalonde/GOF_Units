c8a38e812e6284c3ad67df3a8c0509ce57a5760f
abelarno <abelarno@gmail.com>
2015-11-07 11:30:06 +0100

    Added ctab stuffs

--- Addons/Data/loadouts/ftl.hpp
+++ Addons/Data/loadouts/ftl.hpp
@@ -18,7 +18,7 @@
 		"SmokeShellBlue", "SmokeShellBlue", "SmokeShellBlue", "SmokeShellBlue",
 		"Chemlight_blue",   
 		"rhs_mag_M433_HEDP", "rhs_mag_M433_HEDP", "rhs_mag_M433_HEDP", "rhs_mag_M433_HEDP", "rhs_mag_M433_HEDP",
-		"Laserbatteries",	};
+		"Laserbatteries","itemcTabHCam",	};
 		
 		respawnMagazines[] = {"rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Red","rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Red","rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Red","rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Red","rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Red",
 		"rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Red","rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Red","rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Red", "rhs_mag_30Rnd_556x45_M855A1_Stanag_Tracer_Red", 
@@ -27,7 +27,7 @@
 		"SmokeShellBlue", "SmokeShellBlue", "SmokeShellBlue", "SmokeShellBlue",
 		"Chemlight_blue",   
 		"rhs_mag_M433_HEDP", "rhs_mag_M433_HEDP", "rhs_mag_M433_HEDP", "rhs_mag_M433_HEDP", "rhs_mag_M433_HEDP",
-		"Laserbatteries",	};
+		"Laserbatteries","itemcTabHCam",	};
 		
 		items[] = {MACRO_BasicKit, 
 		"ACE_CableTie", "ACE_CableTie",
@@ -37,9 +37,9 @@
 		"ACE_CableTie", "ACE_CableTie",
 		"ACE_IR_Strobe_Item", "ACE_IR_Strobe_Item",		};
 		
-		linkedItems[] = {"rhsusf_iotv_ocp_Teamleader","rhsusf_ach_helmet_headset_ocp","ItemMap","ItemCompass","tfmicrodagr","tfanprc152_10","ItemGPS","NVGTL"};
+		linkedItems[] = {"rhsusf_iotv_ocp_Teamleader","rhsusf_ach_helmet_headset_ocp","ItemMap","ItemCompass","tfmicrodagr","tfanprc152_10","itemAndroid","NVGTL"};
 		
-		respawnLinkedItems[] = {"rhsusf_iotv_ocp_Teamleader","rhsusf_ach_helmet_headset_ocp","ItemMap","ItemCompass","tfmicrodagr","tfanprc152_10","ItemGPS","NVGTL"};
+		respawnLinkedItems[] = {"rhsusf_iotv_ocp_Teamleader","rhsusf_ach_helmet_headset_ocp","ItemMap","ItemCompass","tfmicrodagr","tfanprc152_10","itemAndroid","NVGTL"};
 		
 		uniformClass = "rhs_uniform_cu_ocp"; //uniform you are using
 		camouflage = 1.6; //If wanting to edit how hard it is for AI to detect it, wouldn't recommend changing this. 1.6 is Default and 2.0 is a sniper ghille 
